12534300586e860073094e97900a3c7d23404f4c
Eva Larumbe <evalarumbe@gmail.com>
2023-11-12 13:04:02 +0100

    Restructure file paths to use src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello World
+        <button id="tr-play">Hello Tone</button>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
