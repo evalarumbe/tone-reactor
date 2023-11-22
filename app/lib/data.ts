456e3041c41fb2ca6cc3b1b58df01bc9aa91080f
Eva Larumbe <evalarumbe@gmail.com>
2023-11-22 04:26:26 +0100

    WIP: Start writing wrapper func for local fetch

---
+++ app/lib/data.ts
@@ -0,0 +1,7 @@
+import { Response } from 'next/server';
+
+export function fetchLocalRoute(endpoint) {
+  console.log(response);
+  // TODO: this is gonna be a wrapper right? so it should return a func that populates some of the request for a new fetch call to the backend.
+  // maybe this func accepts another request, and uses that req to make a new req to the backend, then returns the response that this func gets from the back.
+}
