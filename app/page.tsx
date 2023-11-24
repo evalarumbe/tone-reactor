d40e547c0828f6d4b63f172f8dab58c06ce710f8
Eva Larumbe <evalarumbe@gmail.com>
2023-11-24 11:03:47 +0100

    Tidy homepage styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,16 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex pb-3">
         <h1 className="self-start text-xl font-bold">Tone Reactor</h1>
       </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex pb-2">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex pb-2 border-b-2 border-black">
         <Link className="text-lg font-bold hover:text-gray-600" href="about">About</Link>
         <TonePlay />
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols- lg:text-left h-40">
+      <div className="mt-4 mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
         <h2 className="pb-2">Can we get a random image to show up here?</h2>
+        {/* <Sample /> */}
       </div>
       <div className="mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
-        <h2 className="pb-2">Here's an example of an image that shows up fine:</h2>
+        <h2 className="mt-8 pb-2">Here's an example of an image with remote src that shows up fine:</h2>
         <Image 
           className='lg:w-full'
           src="https://user-images.githubusercontent.com/19648700/284762217-de5b3c2a-108e-4f2f-b4d7-6f9b292c8d75.png"
