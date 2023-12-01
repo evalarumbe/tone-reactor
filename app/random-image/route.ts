704c8b396ea0bcc9c12b5c4ce2b4f5ffebf5bf3c
Eva Larumbe <evalarumbe@gmail.com>
2023-12-01 22:45:11 +0100

    Tidy comments

--- app/random-image/route.ts
+++ app/random-image/route.ts
@@ -1,13 +1,12 @@
 import 'server-only';
 import { NextRequest, NextResponse } from 'next/server';
 
-// I want a client component to programmatically call this endpoint
 export async function GET(request: NextRequest) {
   // fetch a blob from external api
   const res = await fetch(
     'https://api.api-ninjas.com/v1/randomimage?category=abstract',
     {
-      headers: { // TODO: TS error: Handle the undefined case: https://stackoverflow.com/questions/65296563/type-undefined-is-not-assignable-to-type-string-string
+      headers: {
         'X-Api-Key': process.env.RANDOMIMAGE_API_KEY || '',
         'Accept': 'image/jpg',
       }
